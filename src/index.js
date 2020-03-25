87541d829b6d3109ee2d142410be6873653a33c2
Carlos <carlos.delgado@hotmail.it>
2020-03-25 19:48:40 +0100

    adding axios

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import configureStore from './state/store/configureStore'
+import configureStore from './state/store/configureStore';
+import axios from 'axios'
 
 axios.defaults.baseURL = "http://localhost:3000/api/v1";
 
