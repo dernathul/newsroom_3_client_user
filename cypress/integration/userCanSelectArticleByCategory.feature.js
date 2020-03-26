934215cce23e38ed27aa801a797495298e82ddd7
emtalen <59577366+emtalen@users.noreply.github.com>
2020-03-26 17:20:00 +0100

    adds home button in categoryheader and adds more to the test

--- cypress/integration/userCanSelectArticleByCategory.feature.js
+++ cypress/integration/userCanSelectArticleByCategory.feature.js
@@ -19,6 +19,12 @@ describe("User can select article by category", () => {
     cy.get("#article-list").should("contain", "This is a smashing title");
     cy.get("#article-list").should("not.contain", "Zero infected on Mars");
     cy.get("#article-list").should("not.contain", "Lau new president");
-
+    cy.get("#open-article").click();
+    cy.get("#single-article").should("contain", "This is a smashing title");
+    cy.get("h5").should("contain", "And this is an awesome snippet");
+    cy.get("p").should("contain", "And this is the best content you ever read");
+    cy.get("#article-list").should("not.exist");
+    cy.get("#home-button").click();
+    cy.get("#article-list").should("contain", "This is a smashing title");
   });
 });
