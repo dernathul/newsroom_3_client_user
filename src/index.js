6d14c679a31e9baf3516aae39fa99294e0b19db7
Janko <mailodjanka@gmail.com>
2020-03-29 11:00:52 +0200

    Adds stripe elements with yarn, place stripe script in html file, wraps App component into StripeProvider

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import configureStore from "./state/store/configureStore";
 import axios from "axios";
 import "semantic-ui-css/semantic.min.css";
 import { BrowserRouter } from "react-router-dom";
+import { StripeProvider } from "react-stripe-elements"
 
 axios.defaults.baseURL = "http://localhost:3000/api/v1";
 
@@ -15,9 +16,11 @@ const store = configureStore();
 window.store = store;
 ReactDOM.render(
   <Provider store={store}>
+    <StripeProvider apiKey='pk_test_80OAV5NYF42UKxVAzu8MRvzI00eg82xAQ3'>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </StripeProvider>
   </Provider>,
   document.getElementById("root")
 );
