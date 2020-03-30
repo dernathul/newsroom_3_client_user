424220b6be7c4df1b86ca6b9e6a237683a0086ba
Janko <mailodjanka@gmail.com>
2020-03-30 21:03:53 +0200

    Adds a onLogin method

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
-import auth from '../modules/auth'
+import React from "react";
+import auth from "../modules/auth";
 
-const LoginForm = () => {
+const LoginForm = props => {
+  onLogin = async e => {
+    e.preventDefault();
+    try {
+      let response = await auth.signIn(
+        e.target.elements.email.value,
+        e.target.elements.password.value
+      );
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div>
-      <form>
-        <input placeholder="Email" />
-        <input type="password" placeholder="Password" />
+      <form onSubmit={this.onLogin}>
+        <input name="email" placeholder="Email" />
+        <input name="password" type="password" placeholder="Password" />
         <button type="submit">Sign in </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
--- src/modules/auth.js
+++ src/modules/auth.js
@@ -2,7 +2,7 @@ import JtockAuth from "j-tockauth";
  
 const auth = new JtockAuth({
   host: "https://newsroom3api.herokuapp.com",
-  prefixUrl: "/api/v1/auth",
+  prefixUrl: "/api/v1",
 });
  
 export default auth;
