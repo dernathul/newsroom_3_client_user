50ce9a103d778a90d88b8e5b37a959f1731da7d1
Janko <mailodjanka@gmail.com>
2020-03-31 15:44:32 +0200

    Adds an ternary operator to show not logged in users that they need to log in to subscribe and if they're logged in they have the option to subscribe

--- src/components/RestrictedContent.jsx
+++ src/components/RestrictedContent.jsx
@@ -1,21 +1,48 @@
 import React from "react";
 import { connect } from "react-redux";
-import {SHOW_SUBSCRIPTION_FORM} from "../state/actions/actionTypes"
-
+import { SHOW_SUBSCRIPTION_FORM } from "../state/actions/actionTypes";
+import { SHOW_LOGIN_FORM } from "../state/actions/actionTypes";
 const RestrictedContent = props => {
   let article = props.singleArticle;
 
   let shortContent = article.content.substring(0, 20) + "...";
+  let currentUser = props.currentUser;
+
+  let switchLoginAndSubscribe =
+    currentUser.role === "reg_user" ? (
+      <>
+      <p>This is premium article. If you want to continue reading, you need to subscribe.</p>
+      <button
+        onClick={() =>
+          props.dispatch({
+            type: SHOW_SUBSCRIPTION_FORM,
+            payload: { showForm: true }
+          })
+        }
+      >
+        Buy Subscription
+      </button>
+      </>
+    ) : (
+      <>
+      <p>This is premium article. If you want to subscribe, you need to be a registered user</p>
+      <button id = "login-button"
+      onClick = {() => props.dispatch({type: SHOW_LOGIN_FORM, payload: {showLoginForm: true}})}>Login</button>
+    </>
+    );
+
   return (
     <>
       <p id="content">{shortContent}</p>
-      <button onClick = {() => props.dispatch({type: SHOW_SUBSCRIPTION_FORM, payload: {showForm: true}})} >Buy Subscription</button></>
+      {switchLoginAndSubscribe}
+    </>
   );
 };
 
 const mapStateToProps = state => {
   return {
-    singleArticle: state.singleArticle
+    singleArticle: state.singleArticle,
+    currentUser: state.currentUser
   };
 };
 
