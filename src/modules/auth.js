943139bf1e7c657e22cdd72b5334499f3609fef0
emtalen <59577366+emtalen@users.noreply.github.com>
2020-04-01 17:54:03 +0200

    changin back the base url to heroku

--- src/modules/auth.js
+++ src/modules/auth.js
@@ -1,8 +1,8 @@
 import JtockAuth from "j-tockauth";
  
 const auth = new JtockAuth({
-  host: "http://localhost:3000",
-  prefixUrl: "/api/v1",
+  host: "https://newsroom3api.herokuapp.com",
+  prefixUrl: "/api/v1"
 });
  
 export default auth;
